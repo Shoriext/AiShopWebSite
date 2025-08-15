45f62b407d385fad378d5be6a7df2ab3049c7fca
Samadar1 <urapol18@gmail.com>
2025-08-15 20:11:15 +0300

    add: loki & Grafana & prometheus

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM ubuntu:latest
-LABEL authors="urapo"
-
-ENTRYPOINT ["top", "-b"]
+# Используем OpenJDK 17 как базовый образ
+FROM openjdk:17-jdk-slim
+# Указываем рабочую директорию
+WORKDIR /app
+# Копируем JAR файл из target
+COPY target/AiShopWebSite-0.0.1-SNAPSHOT.jar app.jar
+# Запускаем приложение
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/ru/onlinestore/aishopwebsite/AiShopWebSiteApplicationTests.java
+++ src/test/java/ru/onlinestore/aishopwebsite/AiShopWebSiteApplicationTests.java
@@ -1,9 +1,16 @@
 package ru.onlinestore.aishopwebsite;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // или просто отключить
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class AiShopWebSiteApplicationTests {
 
     @Test
